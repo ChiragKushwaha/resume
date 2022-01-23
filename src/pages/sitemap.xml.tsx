723e2917db88127011de65d175be3a33e299d8ec
Chirag Kushwaha <chirag@ip-192-168-0-169.ap-south-1.compute.internal>
2022-01-23 16:06:34 +0530

    type correction

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,8 +1,9 @@
+import { GetServerSidePropsContext } from 'next';
 import React from 'react';
 
 const Sitemap = () => {};
 
-export const getServerSideProps = ({ res }) => {
+export const getServerSideProps = ({ res }: GetServerSidePropsContext) => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
