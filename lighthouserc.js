2cb2e3bfbe43dac0115482681bbd78e5407e8df3
Chirag Kushwaha <chirag@ip-192-168-29-78.ap-south-1.compute.internal>
2022-02-18 15:59:22 +0530

    assertions removed

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,13 +4,13 @@ module.exports = {
       preset: 'lighthouse:recommended',
       assertions: {
         'uses-rel-preload': 'off',
-        'uses-rel-preconnect': 'off',
-        'categories:performance': ['error', { minScore: 0.8 }],
-        'first-contentful-paint': ['error', { maxNumericValue: 2000 }],
-        interactive: ['error', { maxNumericValue: 5200 }],
-        'speed-index': ['error', { maxNumericValue: 4300 }],
-        'resource-summary:script:size': ['error', { maxNumericValue: 170000 }],
-        'resource-summary:total:size': ['error', { maxNumericValue: 300000 }]
+        'uses-rel-preconnect': 'off'
+        // 'categories:performance': ['error', { minScore: 0.8 }],
+        // 'first-contentful-paint': ['error', { maxNumericValue: 2000 }],
+        // interactive: ['error', { maxNumericValue: 5200 }],
+        // 'speed-index': ['error', { maxNumericValue: 4300 }],
+        // 'resource-summary:script:size': ['error', { maxNumericValue: 170000 }],
+        // 'resource-summary:total:size': ['error', { maxNumericValue: 300000 }]
       }
     },
     // Server Side Rendering, hence, it has it's own web server. So, we need to tell Lighthouse how to start the app with next’s built-in server
